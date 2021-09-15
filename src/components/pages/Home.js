8df9ab396456d4c789a893c4815582b056ac598e
Biznaz <aldhelm7@gmail.com>
2021-09-14 20:26:31 -0700

    corrected spelling errors

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => (
   <main>
     <h1>Home</h1>
     <p>
-      Welcome to my portfolio website! I am a new full stack web developer and I will be gradauating soon from the University of Utah's full stack web developer bootcamp. There is a lot to learn in the coding world and the bootcamp was a great chance to abosrb as much as possible. On the front end I have learned how to code HTML, CSS, and JavaScript. I have also learned the back end side of coding including API's, relational and non-realtional databases, and middleware.
+      Welcome to my portfolio website! I am a new full stack web developer and I will be graduating soon from the University of Utah's full stack web developer bootcamp. There is a lot to learn in the coding world and the bootcamp was a great chance to abosrb as much as possible. On the front end I have learned how to code HTML, CSS, and JavaScript. I have also learned the back end side of coding including API's, relational and non-realtional databases, and middleware.
     </p>
     <p>
       Explore my website to learn more about what I have to offer as a Full Stack Web Developer.
